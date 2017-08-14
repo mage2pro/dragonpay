7f7571df70a13dcdc0e20738ae2d195b4877b195
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-14 09:26:08 +0300

    0.1.7

--- Method.php
+++ Method.php
@@ -3,6 +3,19 @@ namespace Dfe\Dragonpay;
 // 2017-04-11
 final class Method extends \Df\PaypalClone\Method {
 	/**
+	 * 2017-08-14
+	 * The amount format is «Numeric(12,2)».
+	 * It is specified in the Chapter 5.2.1.1 «Request Parameters» (page 9) of the PDF documentation:
+	 * https://mage2.pro/t/4259
+	 * @override
+	 * @see \Df\Payment\Method::amountFormat()
+	 * @used-by \Df\Payment\Operation::amountFormat()
+	 * @param float|int $a
+	 * @return string
+	 */
+	function amountFormat($a) {return df_f2($a);}
+
+	/**
 	 * 2017-08-12
 	 * The amount format is «Numeric(12,2)».
 	 * It is specified in the Chapter 5.2.1.1 «Request Parameters» (page 9) of the PDF documentation:
