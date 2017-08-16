2f99670a5e3794dbe4653ed5b12f89181a321534
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-16 17:58:45 +0300

    0.2.0

--- W/Nav.php
+++ W/Nav.php
@@ -1,7 +1,8 @@
 <?php
 namespace Dfe\Dragonpay\W;
+use Dfe\Dragonpay\W\Event as Ev;
 // 2017-08-16
-/** @method \Dfe\Dragonpay\W\Event e() */
+/** @method Ev e() */
 final class Nav extends \Df\PaypalClone\W\Nav {
 	/**
 	 * 2017-08-16
@@ -10,5 +11,10 @@ final class Nav extends \Df\PaypalClone\W\Nav {
 	 * @used-by \Df\PaypalClone\W\Nav::id()
 	 * @return string
 	 */
-	protected function type() {return $this->e()->needChangePaymentState() ? 'capture' : 'info';}
+	protected function type() {return dfc($this, function() {
+		$s = $this->e()->status(); /** @var string $s */
+		return 'S' === $s ? Ev::T_CAPTURE : (in_array($s, ['K', 'R']) ? Ev::T_REFUND : (
+			'A' === $s ? Ev::T_AUTHORIZE : ('V' === $s ? Ev::T_VOID : Ev::T_INFO)
+		))
+	;});}
 }
