2156824074defed48be3e303ffa242e536b30f73
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-13 08:44:28 +0300

    1.1.0

--- W/Responder.php
+++ W/Responder.php
@@ -1,8 +1,7 @@
 <?php
 namespace Dfe\Dragonpay\W;
-use Df\Framework\Controller\Result\Text;
+use Df\Framework\Controller\Response\Text;
 /**
- * 2017-08-14
  * 2017-08-17
  * Currently, Dragonpay does not support the «Authorized», «Chargeback», «Refund» and «Void» responses:
  * https://mage2.pro/t/4295
@@ -12,16 +11,16 @@ use Df\Framework\Controller\Result\Text;
  * please the 0.2.1 version of my extension: https://github.com/mage2pro/dragonpay/tree/0.2.1
  * It is the latest version with these reasponses support.
  */
-final class Handler extends \Df\PaypalClone\W\Handler {
+final class Responder extends \Df\Payment\W\Responder {
 	/**
 	 * 2017-08-14
 	 * «The postback URL handler should return with a simple `content-type:text/plain`
 	 * containing only the single line: `result=OK`.»
 	 * Part 5.2.1.2 «Response Parameters» (page 11) of the PDF documentation: https://mage2.pro/t/4259
 	 * @override
-	 * @see \Df\Payment\W\Handler::result()
-	 * @used-by \Df\Payment\W\Handler::handle()
+	 * @see \Df\Payment\W\Responder::success()
+	 * @used-by \Df\Payment\W\Responder::get()
 	 * @return Text
 	 */
-	protected function result() {return Text::i('result=OK');}
+	protected function success() {return Text::i('result=OK');}
 }
