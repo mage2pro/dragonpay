929487db722d02dde32f6ac53e47474c1b9afb8c
Dmitry Fedyuk <dfediuk@gmail.com>
2019-07-05 13:51:50 +0600

    1.3.2

---
+++ W/Handler.php
@@ -0,0 +1,12 @@
+<?php
+namespace Dfe\Dragonpay\W;
+// 2019-05-07
+final class Handler extends \Df\Payment\W\Handler {
+	/**
+	 * 2019-05-07
+	 * @override
+	 * @see \Df\Payment\W\Handler::strategyC()
+	 * @used-by \Df\Payment\W\Handler::handle()
+	 */
+	protected function strategyC() {return \Dfe\Dragonpay\W\Strategy\ConfirmPending::class;}
+}
---
+++ W/Strategy/ConfirmPending.php
@@ -0,0 +1,23 @@
+<?php
+namespace Dfe\Dragonpay\W\Strategy;
+use Dfe\Dragonpay\W\Event as E;
+/**
+ * 2019-05-07
+ * @used-by \Dfe\Dragonpay\W\Handler::strategyC()
+ * @method E e()
+ */
+final class ConfirmPending extends \Df\Payment\W\Strategy\ConfirmPending {
+	/**
+	 * 2019-07-05
+	 * 1) "Provide an ability to store intermediate responses
+	 * for the `Df\Payment\W\Strategy\ConfirmPending` strategy": https://github.com/mage2pro/core/issues/86
+	 * 2) "Show the `refno` value on the backend order screen for not yet paid orders":
+	 * https://github.com/mage2pro/dragonpay/issues/7
+	 * 3) The default value is false for backward compatibility.
+	 * @override
+	 * @see \Df\Payment\W\Strategy\ConfirmPending::storeIntermediateResponses()
+	 * @used-by \Df\Payment\W\Strategy\ConfirmPending::_handle()
+	 * @return bool
+	 */
+	protected function storeIntermediateResponses() {return true;}
+}
